da1a4b3689e371ac69e22e36a2b7cfed9431a13d
denie2311-commits <denie2311@gmail.com>
2026-03-21 12:10:01 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base:'/B-i-Nguy-n-Th-o-H-ng on main/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
